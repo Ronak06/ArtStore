c69a0244d54c99d45da3b09805a979d23bf5fbfb
Ronak <ronakpatel0609@gmail.com>
2018-03-03 02:13:58 -0500

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var artmodal = document.getElementById("firstArtWork");
+var artmodal = document.getElementById("ArtWorks");
+var artistmodal = document.getElementById("Artists");
+
 function paintingCase()
 {
   document.getElementById("datetitle").innerHTML = 'Date';
@@ -11,80 +13,75 @@ function paintingCase()
 }
 function artistCase()
 {
-  document.getElementById("datetitle").innerHTML = 'Date of Birth';
-  document.getElementById("typetitle").innerHTML = 'Date of Death';
-  document.getElementById("dimensionstitle").innerHTML = 'Place of Living';
-  document.getElementById("locationtitle").innerHTML = 'Genre of Painting';
-  document.getElementById("artisttitle").innerHTML = 'Famous Peices';
-  document.getElementById("pricetitle").innerHTML = '';
-  document.getElementById("genretitle").innerHTML = '';
-  document.getElementById("price").innerHTML = '';
-  document.getElementById("genre").innerHTML = '';
+  document.getElementById("dateofbirth").innerHTML = 'Date of Birth';
+  document.getElementById("dateofdeath").innerHTML = 'Date of Death';
+  document.getElementById("placeofliving").innerHTML = 'Place of Living';
+  document.getElementById("paintinggenre").innerHTML = 'Genre of Painting';
+  document.getElementById("famouspieces").innerHTML = 'Famous Pieces';
 }
 function grantWood()
 {
   artistCase();
-  document.getElementById("modalHeader").innerHTML = "Grant Wood";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/Artists/GrantWood.jpg\' height=\'400\' width=\'300\'/>';
-  document.getElementById("date").innerHTML = 'February 13, 1891';
-  document.getElementById("type").innerHTML = 'February 12, 1942';
-  document.getElementById("dimention").innerHTML = 'America';
-  document.getElementById("location").innerHTML = 'Gothic';
-  document.getElementById("artist").innerHTML = 'American Gothic, Woman with Plants, Young Corn';
-  $(artmodal).modal();
+  document.getElementById("modalHeader1").innerHTML = "Grant Wood";
+  document.getElementById("modalBody1").innerHTML = '<img src=\'Images/Artists/GrantWood.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("datebirth").innerHTML = 'February 13, 1891';
+  document.getElementById("datedeath").innerHTML = 'February 12, 1942';
+  document.getElementById("placeliving").innerHTML = 'Iowa City, Iowa, United States';
+  document.getElementById("genrepainting").innerHTML = 'Gothic';
+  document.getElementById("famepieces").innerHTML = 'American Gothic, Woman with Plants, Young Corn';
+  $(artistmodal).modal();
 
 }
 function leonardo()
 {
   artistCase();
-  document.getElementById("modalHeader").innerHTML = "Leonardo da Vinci";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/Artists/LeonardodaVinci.jpg\' height=\'400\' width=\'300\'/>';
-  document.getElementById("date").innerHTML = 'April 15, 1452';
-  document.getElementById("type").innerHTML = 'May 2, 1519';
-  document.getElementById("dimention").innerHTML = 'Anchiano, Italy';
-  document.getElementById("location").innerHTML = 'Renaissance';
-  document.getElementById("artist").innerHTML = 'Mona Lisa, The Last Supper, Vitruian Man';
-  $(artmodal).modal();
+  document.getElementById("modalHeader1").innerHTML = "Leonardo da Vinci";
+  document.getElementById("modalBody1").innerHTML = '<img src=\'Images/Artists/LeonardodaVinci.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("datebirth").innerHTML = 'April 15, 1452';
+  document.getElementById("datedeath").innerHTML = 'May 2, 1519';
+  document.getElementById("placeliving").innerHTML = 'Clos Lucé, Amboise, France';
+  document.getElementById("genrepainting").innerHTML = 'Renaissance';
+  document.getElementById("famepieces").innerHTML = 'Mona Lisa, The Last Supper, Vitruian Man';
+  $(artistmodal).modal();
 }
 function vincent()
 {
   artistCase();
-  document.getElementById("modalHeader").innerHTML = "Vincent Van Gogh";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/Artists/VincentvanGogh.jpg\' height=\'400\' width=\'300\'/>';
-  document.getElementById("date").innerHTML = 'March 30, 1853';
-  document.getElementById("type").innerHTML = 'July 29, 1890';
-  document.getElementById("dimention").innerHTML = 'France';
-  document.getElementById("location").innerHTML = 'Western Art';
-  document.getElementById("artist").innerHTML = 'The Starry Night, Irises, The Potato Eaters';
-  $(artmodal).modal();
+  document.getElementById("modalHeader1").innerHTML = "Vincent Van Gogh";
+  document.getElementById("modalBody1").innerHTML = '<img src=\'Images/Artists/VincentvanGogh.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("datebirth").innerHTML = 'March 30, 1853';
+  document.getElementById("datedeath").innerHTML = 'July 29, 1890';
+  document.getElementById("placeliving").innerHTML = 'Auvers-sur-Oise, France';
+  document.getElementById("genrepainting").innerHTML = 'Western Art';
+  document.getElementById("famepieces").innerHTML = 'The Starry Night, Irises, The Potato Eaters';
+  $(artistmodal).modal();
 }
 function dali()
 {
   artistCase();
-  document.getElementById("modalHeader").innerHTML = "Salvador Dali";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/Artists/SalvadorDali.jpg\' height=\'400\' width=\'300\'/>';
-  document.getElementById("date").innerHTML = ' May 11, 1904';
-  document.getElementById("type").innerHTML = 'January 23, 1989';
-  document.getElementById("dimention").innerHTML = 'Figueres, Spain';
-  document.getElementById("location").innerHTML = 'Surrealism, Cubism, Modern Art';
-  document.getElementById("artist").innerHTML = 'The Persistence of Memory, The Burning Giraffe, The Great Masturbator';
-  $(artmodal).modal();
+  document.getElementById("modalHeader1").innerHTML = "Salvador Dali";
+  document.getElementById("modalBody1").innerHTML = '<img src=\'Images/Artists/SalvadorDali.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("datebirth").innerHTML = ' May 11, 1904';
+  document.getElementById("datedeath").innerHTML = 'January 23, 1989';
+  document.getElementById("placeliving").innerHTML = 'Figueres, Spain';
+  document.getElementById("genrepainting").innerHTML = 'Surrealism, Cubism, Modern Art';
+  document.getElementById("famepieces").innerHTML = 'The Persistence of Memory, The Burning Giraffe, The Great Masturbator';
+  $(artistmodal).modal();
 }
 function picasso()
 {
   artistCase();
   document.getElementById("modalHeader").innerHTML = "Pablo Picasso";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/Artists/PabloPicasso.jpg\' height=\'400\' width=\'300\'/>';
-  document.getElementById("date").innerHTML = ' October 25, 1881';
-  document.getElementById("type").innerHTML = 'April 8, 1973';
-  document.getElementById("dimention").innerHTML = 'Málaga, Spain';
-  document.getElementById("location").innerHTML = 'Surrealism, Cubism, Modern Art';
-  document.getElementById("artist").innerHTML = 'Guernica, The Old Guitarist, Chicago Picasso';
-  $(artmodal).modal();
+  document.getElementById("modalBody1").innerHTML = '<img src=\'Images/Artists/PabloPicasso.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("datebirth").innerHTML = ' October 25, 1881';
+  document.getElementById("datedeath").innerHTML = 'April 8, 1973';
+  document.getElementById("placeliving").innerHTML = 'Mougins, France';
+  document.getElementById("genrepainting").innerHTML = 'Surrealism, Cubism, Modern Art';
+  document.getElementById("famepieces").innerHTML = 'Guernica, The Old Guitarist, Chicago Picasso';
+  $(artistmodal).modal();
 }
 function monaLisa()
 {
-  //artmodal.img = "Images/monalisa.jpg";
   paintingCase();
   document.getElementById("modalHeader").innerHTML = "Mona Lisa";
   document.getElementById("modalBody").innerHTML = '<img src=\'Images/ArtWork/monalisa.jpg\' height=\'400\' width=\'300\'/>';
@@ -96,7 +93,6 @@ function monaLisa()
   document.getElementById("price").innerHTML = '$225.00';
   document.getElementById("genre").innerHTML = 'Portrait';
   $(artmodal).modal();
-
 }
 function americangothic()
 {
@@ -111,7 +107,6 @@ function americangothic()
   document.getElementById("price").innerHTML = '$103.50';
   document.getElementById("genre").innerHTML = 'Portrait';
   $(artmodal).modal();
-
 }
 function starrynight()
 {
