e9ca169700200b2a7692efeb723ce501a908a977
Ronak <ronakpatel0609@gmail.com>
2018-03-11 12:19:08 -0400

    Added log.php

---
+++ log.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["fname"]; ?><br>
+Your Last Name is: <?php echo $_POST["lname"]; ?>
+Your Address is: <?php echo $_POST["address"]; ?>
+Your Postal Code is: <?php echo $_POST["postalcode"]; ?>
+Your City is: <?php echo $_POST["city"]; ?>
+Your Email Address is: <?php echo $_POST["email"]; ?>
+Your Phone Number is: <?php echo $_POST["phone"]; ?>
+
+</body>
+</html>
